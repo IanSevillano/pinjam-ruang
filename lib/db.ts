b28654cc7c905bc31e9297b3f5e3075d5cb36085
M Arfazzuhaili <arfaprapen@gmail.com>
2025-09-02 08:54:15 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,14 @@
-// lib/db.ts
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_pinjam_ruangan',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // wajib kalau pakai Aiven
+  },
 });
 
 export default db;
